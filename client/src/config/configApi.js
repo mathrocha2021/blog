dfc7d75d115e52c5063e3853fc978c5d2af0c73a
unknown <mathrocha2021@gmail.com>
2023-11-21 11:04:16 -0300

    .env

--- client/src/config/configApi.js
+++ client/src/config/configApi.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
 // Define a URL base para todas as requisições feitas com esta instância
+const baseURL = process.env.REACT_APP_API_URL;
+
+// Define a URL base para todas as requisições feitas com esta instância
 const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: baseURL
 });
 
 export default api;
